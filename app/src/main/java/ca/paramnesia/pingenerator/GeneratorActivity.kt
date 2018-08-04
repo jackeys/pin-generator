acf391be36be0cb4e06f578850365d34c86152bc
Jack Keys <jackeys@live.ca>
2018-08-04 17:12:30 -0600

    Create an empty activity Android project

---
+++ app/src/main/java/ca/paramnesia/pingenerator/GeneratorActivity.kt
@@ -0,0 +1,12 @@
+package ca.paramnesia.pingenerator
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class GeneratorActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_generator)
+    }
+}
