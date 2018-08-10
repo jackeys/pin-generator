98f845aa7375d3149dee4548af80191a7622f80e
Jack Keys <jackeys@live.ca>
2018-08-10 05:40:00 -0600

    Set minimum PIN length to 3 in the UI
    This is a work-around, since the min attribute wasn't added until API 26

--- app/src/main/java/ca/paramnesia/pingenerator/GeneratorActivity.kt
+++ app/src/main/java/ca/paramnesia/pingenerator/GeneratorActivity.kt
@@ -11,22 +11,26 @@ import kotlinx.android.synthetic.main.activity_generator.*
 class GeneratorActivity : AppCompatActivity() {
     private lateinit var viewModel: GeneratorViewModel
 
+    private val minimumLength: Int = 3
+    private val SeekBar.adjustedProgress: Int
+        get() = progress + minimumLength
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_generator)
         viewModel = ViewModelProviders.of(this).get(GeneratorViewModel::class.java)
 
-        lengthText.text = lengthSeekBar.progress.toString()
+        lengthText.text = lengthSeekBar.adjustedProgress.toString()
         lengthSeekBar.setOnSeekBarChangeListener(object: SeekBar.OnSeekBarChangeListener {
             override fun onProgressChanged(bar: SeekBar?, value: Int, fromUser: Boolean) {
-                lengthText.text = value.toString()
+                lengthText.text = (value + minimumLength).toString()
             }
 
             override fun onStartTrackingTouch(bar: SeekBar?) {}
             override fun onStopTrackingTouch(bar: SeekBar?) {}
         })
 
-        generateButton.setOnClickListener { viewModel.requestNewPin(lengthSeekBar.progress) }
+        generateButton.setOnClickListener { viewModel.requestNewPin(lengthSeekBar.adjustedProgress) }
 
         viewModel.pin.observe(this, Observer { pin ->
             pinText.text = pin?.toString()
