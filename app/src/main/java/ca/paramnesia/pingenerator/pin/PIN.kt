34ea94716cb5cd404c12268c78030b54eb9c2aed
Jack Keys <jackeys@live.ca>
2018-08-11 22:04:08 -0600

    Add a convenience method for creating pins from a list of integers

--- app/src/main/java/ca/paramnesia/pingenerator/pin/PIN.kt
+++ app/src/main/java/ca/paramnesia/pingenerator/pin/PIN.kt
@@ -1,6 +1,10 @@
 package ca.paramnesia.pingenerator.pin
 
 data class PIN(val digits: List<Digit>) {
+    companion object {
+        fun from(digits: List<Int>) = PIN(digits.map { Digit(it) })
+    }
+
     override fun toString() = digits.fold("") { string, digit ->
             string + digit.toString()
         }
--- app/src/test/groovy/ca/paramnesia/pingenerator/pin/PinTest.groovy
+++ app/src/test/groovy/ca/paramnesia/pingenerator/pin/PinTest.groovy
@@ -1,13 +1,42 @@
 package ca.paramnesia.pingenerator.pin
 
 import spock.lang.Specification
-import spock.lang.Unroll
 
 class PinTest extends Specification {
-    @Unroll
-    def "PIN #expected_pin prints correctly"() {
+    def "PIN generated correctly from array of integers"() {
+        when: "Creating a PIN with a list of single-digit integers"
+        def pin = PIN.@Companion.from(digits)
+
+        then: "The list of digits should be the same as the list of integers"
+        pin.digits == digits
+
+        where:
+        digits << [
+                [4, 1, 6, 7, 3, 5, 8],
+                [7, 7, 7],
+                [0, 1, 5, 7, 3, 8, 5, 3, 2, 4]
+        ]
+    }
+
+    def "An exception is thrown if trying to create a PIN from non-digit integers"() {
+        when: "Creating a PIN with a list of non-digit integers"
+        PIN.@Companion.from(numbers)
+
+        then:
+        thrown IllegalArgumentException
+
+        where:
+        numbers << [
+                [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+                [76543456,3, 7, 5],
+                [-1, 4, 7, 4, 5],
+                [5, 9, 1, 3, 5, -19473933]
+        ]
+    }
+
+    def "PIN prints correctly"() {
         when: "Creating a PIN with a list of digits"
-        def pin = new PIN(digits.collect { num -> new Digit(num) })
+        def pin = PIN.@Companion.from(digits)
 
         then: "The digits should be printed naturally, in order"
         pin.toString() == expected_pin
