412ce97a1b8bdf741d8497b117c454ae7db3b264
Jack Keys <jackeys@live.ca>
2018-08-12 01:53:30 -0600

    Add documentation to StepFilter

--- app/src/main/java/ca/paramnesia/pingenerator/pin/StepFilter.kt
+++ app/src/main/java/ca/paramnesia/pingenerator/pin/StepFilter.kt
@@ -2,6 +2,15 @@ package ca.paramnesia.pingenerator.pin
 
 import kotlin.math.max
 
+/**
+ * Filters out PINs based on finding patterns in ascending or descending values.
+ * This is based on finding the same step sequence (e.g. every digit is 1 higher than the last,
+ * like 1234, or every digit is 2 lower than the last, like 9753, etc). Note that digits are
+ * considered to "wrap around", so the pattern 8901 would fail in the same way as the pattern 0123.
+ *
+ * There is a tolerance that depends on the length of the PIN. For example, while the
+ * PIN 123 is very insecure, the PIN 123601837949 does not suffer from the same insecurity.
+ */
 class StepFilter: Filter {
     companion object {
         // If there is only one step, there is no pattern - we need at least two
