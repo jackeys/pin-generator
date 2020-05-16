c2c90abf3c4d676a038b515cbe60f4cf535da54a
Jack Keys <jackeys@live.ca>
2020-05-15 22:39:11 -0600

    Use step filter when generating PINs

--- app/src/test/groovy/ca/paramnesia/pingenerator/pin/PinFactoryTest.groovy
+++ app/src/test/groovy/ca/paramnesia/pingenerator/pin/PinFactoryTest.groovy
@@ -35,12 +35,31 @@ class PinFactoryTest extends Specification {
         randomMock.nextInt(_) >>> sequence
 
         when: "Generating a PIN using our known sequence"
-        def pin = new PinFactory(randomMock).generatePin(length)
+        def pin = new PinFactory(new StepFilter(), randomMock).generatePin(length)
 
         then: "The PIN should match the sequence up to our length"
         pin.digits == sequence[0..<length]
 
         where:
         length << [1, 4, 8, 12]
+    }
+
+    def "PINs that fail filter aren't returned"() {
+        setup: "A known sequence of values for three PINs"
+        def failedPIN1 = [1, 1, 1, 1]
+        def failedPIN2 = [1, 2, 3, 4]
+        def passedPIN = [6, 3, 7, 7]
+        Random randomMock = Mock()
+        randomMock.nextInt(_) >>> failedPIN1 + failedPIN2 + passedPIN
+
+        and: "A filter that will fail the first two PINs"
+        Filter filterMock = Mock()
+        filterMock.verify(_) >>> [false, false, true]
+
+        when: "The first two PINs fail the filter"
+        def pin = new PinFactory(filterMock, randomMock).generatePin(4)
+
+        then: "The PIN should match the passed PIN"
+        pin.digits == passedPIN
     }
 }
